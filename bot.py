29d79ea62e60a065968f3c723bd2e5b54f3fdee3
jeissonquimbayaf-dev <jeisson.quimbayaf@gmail.com>
2026-07-30 12:56:54 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,11 +58,10 @@ def enviar_alerta():
        - **Aerolínea y Precio:** (Aerolínea - Precio aprox. en COP)
 
     2. 📊 **Resumen de tarifas y horarios alternativos:**
-    La mejor tarifa y precio y horario del vuelo de ida (con la aerolínea conseguida)
-    La mejor tarifa y precio horario del vuelo de regreso (con la aerolínea conseguida)
+    La mejor tarifa y precio y horario del mejor precio del vuelo de ida (con la aerolínea conseguida)
+    La mejor tarifa y precio horario del mejor precio del vuelo de regreso (con la aerolínea conseguida)
 
-    Si la aerolínea es diferente, poner también la mejor tarifa y horario en una sola aerolínea. 
-    
+       
     3. 🔗 **Enlaces para consultar / comprar:**
     4. Un aviso sobre restricciones comunes (tarifas básicas, equipaje no incluido, etc.).
     5. 💡 **Tendencia/Recomendación:**
